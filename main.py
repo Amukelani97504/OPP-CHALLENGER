d2a512d0db64a217175d6dcf57d491acaaa82062
amukelaningo@gmail.com <amukelaningo@gmail.com>
2025-05-03 19:17:09 +0200

    Added my Digital Pet challenge project

---
+++ main.py
@@ -0,0 +1,14 @@
+from pet import Pet
+
+# Create an instance of the Pet class
+my_pet = Pet("Fluffy")
+
+# Interact with the pet
+my_pet.get_status()
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+my_pet.train("roll over")
+my_pet.train("fetch")
+my_pet.show_tricks()
+my_pet.get_status()
